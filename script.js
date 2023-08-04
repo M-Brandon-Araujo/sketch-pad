588e621753671bde5ec3512be4abc14f5bc948de
Brandon Araujo <brandonaraujo077@gmail.com>
2023-08-04 12:51:12 -0500

    Add functionality for buttons. Create functions that handle color changes. Create function that generates random rgb values for rainbow button.

--- script.js
+++ script.js
@@ -1,22 +1,102 @@
 const canvas = document.querySelector('.canvas');
-createGrid(16);
+const canvasChildren = canvas.childNodes;
+const btn = document.querySelectorAll('.button');
+const colorPicker = document.getElementById('colorPicker');
+let gridSize = 50;
+let currentColor = 'black';
 
-function createGrid(cellNumber) {
-    let cellNumberSquared = cellNumber * cellNumber;
-    for (let i = 1; i <= cellNumberSquared; i++) {
-        const cell = document.createElement('div');
-        canvas.appendChild(cell);
-        cell.classList.add('cell');
-        cell.style.cssText = `height: ${600 / cellNumber}px; width: ${600 / cellNumber}px;`;
+//buttons
+btn.forEach(buttonEvent, this);
+
+function buttonEvent(element) {
+    element.addEventListener('click', function(e) {
+        let newText = element.textContent.toLowerCase();
+        switch (newText) {
+            case 'eraser':
+                eraserFunction();
+                break;
+            case 'rainbow': 
+                rainbowFunction();
+                break;
+            case 'large':
+                clearGrid()
+                createGrid(100);
+                break;
+            case 'medium':
+                clearGrid();
+                createGrid(50);
+                break;
+            case 'small':
+                clearGrid();
+                createGrid(20);
+                break;
+        }
+    })
+}
+
+//change color
+colorPicker.addEventListener('input', (e) => {
+    let newColor = colorPicker.value;
+    currentColor = newColor;
+    canvasChildren.forEach((element) => {
+        element.addEventListener('mouseover', function(e) {
+            element.style.backgroundColor = `${newColor}`;
+        })
+    })
+})
+
+//rainbow
+function rainbowFunction() {
+    canvasChildren.forEach((element) => {
+        element.addEventListener('mouseover', function(e) {
+            element.style.backgroundColor = `rgb(${randomRgb()}, ${randomRgb()}, ${randomRgb()})`;
+        })
+    })
+}
+
+//random rgb values
+function randomRgb() {
+    min = Math.ceil(0);
+    max = Math.floor(255);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(randomRgb());
+
+//eraser
+function eraserFunction() {
+    canvasChildren.forEach((element) => {
+        element.addEventListener('mouseover', function(e) {
+            element.style.backgroundColor = 'white';
+        })
+    })
+}
+
+//clear grid
+function clearGrid() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
     }
 }
 
-const theCells =document.querySelectorAll('.cell');
+createGrid(gridSize);
 
-theCells.forEach(element => mouseOverFunction(element));
+//grid creation
+function createGrid(gridSize) {
+    let gridSizeSquared = gridSize * gridSize;
+    for (let i = 1; i <= gridSizeSquared; i++) {
+        const cell = document.createElement('div');
+        canvas.appendChild(cell);
+        cell.classList.add('cell');
+        cell.style.cssText = `height: ${600 / gridSize}px; width: ${600 / gridSize}px;`;
+    }
+    const theCells =document.querySelectorAll('.cell');
+    theCells.forEach(element => mouseOverFunction(element));
+}
 
+//initialize pen color
 function mouseOverFunction(element) {
     element.addEventListener('mouseover', function(e) {
-        element.classList.add('hovered');
+            element.style.backgroundColor = currentColor;
     })
 }
